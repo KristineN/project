2ecbbbaa81776eab27deb1a32786ed4b78a58467
KristineN <kristinenyunko@gmail.com>
2016-08-27 16:32:00 +0300

    project

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,20 @@
-grunt.loadNpmTasks('grunt-express-server');
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-express-server');
 
 grunt.initConfig({
   express: {
     options: {
-      port: 5000,
+      // Override defaults here
     },
     dev: {
       options: {
-        script: 'src/index.html'
+        script: 'src/server.js'
       }
     }
   }
 });
 
 grunt.registerTask('start', [ 'express:dev']);
+
+};
