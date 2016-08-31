be135a3b361932a21fcf8749a1c298587c4fb394
KristineN <kristinenyunko@gmail.com>
2016-08-31 14:17:36 +0300

    project

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,20 @@
 
 
-document.getElementById('button').click = a;
+/*document.getElementById('button').click = a;
 
 		var a = function(but) {
 			console.log("Hello");
 		};
 
 		if( document.body.attachEvent)
-			document.getElementById('button').attachEvent("click", a);
+			document.getElementById('button').attachEvent("click", a);*/
+
+
+var el = document.getElementById('button')
+	clickerFn = function(but) {
+		console.log('Hello world');
+	}
+
+	el.addEventListener('click', clickerFn);
+
+
