3adf8a517319eae4b18f8cad1b17e732b22370b6
KristineN <kristinenyunko@gmail.com>
2016-09-01 12:48:21 +0300

    project

--- src/js/search.js
+++ src/js/search.js
@@ -3,28 +3,40 @@ var getMovieData = document.getElementById('button')
 
 	clickerFn = function(but) {
 
-		console.log('Hello world');
+		console.log('search');
+
+			return search();
 	}
 
 getMovieData.addEventListener('click', clickerFn);
 
 
 
-function run() {
+
+
+function search() {
 
 	var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://www.gismeteo.ua/', true);
 
-xhr.send();
+xhr.send(null);
 
-xhr.onreadystatechange = function(but) {
-	if (this.readyState  = 4) return;
+xhr.onreadystatechange = function() {
+	if (this.readyState  == 4) return;
 
-	if (this.status  = 200) {
+
+
+	if (this.status  == 200) {
 		alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
 		return;
-	
-  		}
-	}
+
+		alert(xhr.onreadystatechange);
+
+
+   }
+
 }
+
+}
+ 
